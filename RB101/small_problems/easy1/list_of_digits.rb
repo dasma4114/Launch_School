1f12ab6eab5d06335ecc868224b50b4223718e9d
JP <67994969+dasma4114@users.noreply.github.com>
2021-09-12 08:05:38 -0700

    2nd easy problem

---
+++ RB101/small_problems/easy1/list_of_digits.rb
@@ -0,0 +1,8 @@
+#Write a method that takes one argument, a positive integer, and returns a list of the digits in the number.
+
+def digit_list(num)
+  numsplit = num.to_s.split(//)
+  numsplit.map { |str| str.to_i }
+end
+
+print digit_list(12345)
