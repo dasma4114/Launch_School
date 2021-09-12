b1c211456c81f89833e96c2775f17f6172c2a8a3
JP <67994969+dasma4114@users.noreply.github.com>
2021-09-11 23:17:01 -0700

    Delete odd.rb

--- RB101/small_problems/odd.rb
+++
@@ -1,9 +0,0 @@
-#Write a method that takes one integer argument, which may be positive, negative, or zero. This method returns true if the number's absolute value is odd. You may assume that the argument is a valid integer value.
-
-def odd(num)
-  num.odd?
-end
-
-p odd(2)
-p odd(-3)
-p odd(0)
